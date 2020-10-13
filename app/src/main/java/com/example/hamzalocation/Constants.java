b64ab0cfbbbb45375e1a9228189782a77f1f5389
Mahad <mahadmunir10@gmail.com>
2020-10-13 18:02:50 +0500

    initial commit

---
+++ app/src/main/java/com/example/hamzalocation/Constants.java
@@ -0,0 +1,8 @@
+package com.example.hamzalocation;
+
+public class Constants {
+    static final int Location_Service_ID = 175;
+    static final String ACTION_START_LOCATION_SERVICE = "startLocationService";
+    static final String ACTION_STOP_LOCATION_SERVICE = "stopLocationService";
+
+}
---
+++ app/src/main/java/com/example/hamzalocation/LocationService.java
@@ -0,0 +1,160 @@
+package com.example.hamzalocation;
+
+import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
+import android.os.IBinder;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import pk.codebase.requests.HttpError;
+import pk.codebase.requests.HttpRequest;
+import pk.codebase.requests.HttpResponse;
+
+public class LocationService extends Service {
+    public LocationService() {
+    }
+
+    private LocationCallback locationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            super.onLocationResult(locationResult);
+            if (locationResult != null && locationResult.getLastLocation() != null) {
+                double latitude = locationResult.getLastLocation().getLatitude();
+                double longitude = locationResult.getLastLocation().getLongitude();
+                Log.d("Location_Update", latitude + "," + longitude);
+                sendLocation(latitude,longitude);
+            }
+        }
+
+        @Override
+        public void onLocationAvailability(LocationAvailability locationAvailability) {
+            super.onLocationAvailability(locationAvailability);
+        }
+    };
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    private void startLocationUpdates() {
+        String channel_Id = "location_notification_channel";
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        Intent resuultIntent = new Intent();
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                getApplicationContext(),
+                0,
+                resuultIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+        );
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(
+                getApplicationContext(), channel_Id
+        );
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setContentTitle("Location Service");
+        builder.setDefaults(NotificationCompat.DEFAULT_ALL);
+        builder.setContentText("Running");
+        builder.setContentIntent(pendingIntent);
+        builder.setAutoCancel(false);
+        builder.setPriority(NotificationCompat.PRIORITY_MAX);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            if (notificationManager != null &&
+                    notificationManager.getNotificationChannel(channel_Id) == null) {
+                NotificationChannel notificationChannel = new NotificationChannel(
+                        channel_Id, "Location Service ",
+                        NotificationManager.IMPORTANCE_HIGH
+                );
+                notificationChannel.setDescription("This channel is used by location service");
+                notificationManager.createNotificationChannel(notificationChannel);
+            }
+        }
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(4000);
+        locationRequest.setFastestInterval(2000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        LocationServices.getFusedLocationProviderClient(this)
+                .requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
+        startForeground(Constants.Location_Service_ID,builder.build());
+    }
+
+    private void stopLocationService(){
+        LocationServices.getFusedLocationProviderClient(this)
+                .removeLocationUpdates(locationCallback);
+        stopForeground(true);
+        stopSelf();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (intent != null){
+            String action = intent.getAction();
+            if (action != null){
+                if (action.equals(Constants.ACTION_START_LOCATION_SERVICE)){
+                    startLocationUpdates();
+                }else if (action.equals(Constants.ACTION_STOP_LOCATION_SERVICE)){
+                    stopLocationService();
+                }
+            }
+        }
+        return super.onStartCommand(intent, flags, startId);
+    }
+    private void sendLocation(final double latitude, final double longitude) {
+        List<Object> args = new ArrayList<>();
+        args.add(latitude);
+        args.add(longitude);
+
+        HttpRequest request = new HttpRequest();
+        request.setOnResponseListener(new HttpRequest.OnResponseListener() {
+            @Override
+            public void onResponse(HttpResponse response) {
+                Toast.makeText(LocationService.this, "Success", Toast.LENGTH_SHORT).show();
+                Toast.makeText(LocationService.this, String.valueOf(latitude) + ", " + String.valueOf(longitude),
+                      Toast.LENGTH_SHORT).show();
+            }
+        });
+        request.setOnErrorListener(new HttpRequest.OnErrorListener() {
+            @Override
+            public void onError(HttpError error) {
+                Toast.makeText(LocationService.this, "Fail", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Map<String, Object> payload = new HashMap<>();
+        payload.put("topic", "pk.codebase.hamza.location");
+        payload.put("args", args);
+
+        request.post("http://codebase.pk:8080/publish", payload);
+    }
+}
